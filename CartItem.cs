bbfb2e2101db6eaa2e4c442a926ffde0d7e72e28
JUSTINEMALIT <zuluetajustine92@gmail.com>
2026-04-28 11:13:12 +0800

    add some constructors and categories helpers first and try to seperate the files to be more readable and optimize

---
+++ CartItem.cs
@@ -0,0 +1,28 @@
+using System;
+
+
+
+class CartItem
+{
+    // Properties
+    public Food   Product  { get; set; }
+    public int    Quantity { get; set; }
+
+    // Subtotal ay naka auto-computed from Product price x Quantity
+    public double Subtotal => Product.GetItemTotal(Quantity);
+
+    public CartItem(Food product, int quantity)
+    {
+        Product  = product;
+        Quantity = quantity;
+    }
+
+    //Display
+    public void Display(int index)
+    {
+        Console.WriteLine(
+            $"  [{index}] {Product.Name,-26} x{Quantity,3}" +
+            $"   PHP {Product.Price,8:F2}   Subtotal: PHP {Subtotal,8:F2}"
+        );
+    }
+}
